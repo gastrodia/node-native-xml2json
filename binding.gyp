516bca98ebad1f56f033d5b1c66a58b5e4ee2f9b
xuyang <xuyang@wozlla.com>
2015-07-29 09:31:12 +0800

    使用nan模块修复兼容性问题

--- binding.gyp
+++ binding.gyp
@@ -3,6 +3,9 @@
     {
       'target_name': 'xml_parser',
       'sources': [ 'src/xml_parser.cc' ],
+       "include_dirs" : [
+ 	 			"<!(node -e \"require('nan')\")"
+			],
       "cflags!": [ '-fno-exceptions' ],
       "cflags_cc!": [ '-fno-exceptions' ],
       "conditions": [
--- src/xml_parser.cc
+++ src/xml_parser.cc
@@ -1,5 +1,6 @@
 #include <node.h>
 #include <v8.h>
+#include <nan.h>
 #include <unistd.h>
 #include <string>
 #include "xml2json/xml2json.hpp"
@@ -8,27 +9,38 @@ using namespace v8;
 using namespace std;
 
 
-Handle<Value> ReadXMLStrAsJSONStr(const Arguments& args) {
-  HandleScope scope;
+// Handle<Value> ReadXMLStrAsJSONStr(const Arguments& args) {
+//   HandleScope scope;
+//
+// char *c_arg, *ret;
+//
+//  if (args.Length() < 1) {
+//    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
+//    return scope.Close(Undefined());
+//  }
+//
+//  c_arg = *v8::String::Utf8Value(args[0]->ToString());
+//
+//  ret = c_arg; //do_sqlsig(c_arg);
+//
+//   string json_str = xml2json(ret);
+//
+//   return scope.Close(String::New(json_str.c_str()));
+// }
 
-char *c_arg, *ret;
+NAN_METHOD(ReadXMLFunction) {
+    NanScope();
 
- if (args.Length() < 1) {
-   ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-   return scope.Close(Undefined());
- }
+    NanUtf8String* location = new NanUtf8String(args[0]);
 
- c_arg = *v8::String::Utf8Value(args[0]->ToString());
-
- ret = c_arg; //do_sqlsig(c_arg);
-
-  string json_str = xml2json(ret);
-
-  return scope.Close(String::New(json_str.c_str()));
+    string json_str = xml2json(location->operator*());
+    NanReturnValue(json_str);
 }
 
-void init(Handle<Object> target) {
-  NODE_SET_METHOD(target, "xml2json", ReadXMLStrAsJSONStr);
+void init(Handle<Object> exports) {
+  //NODE_SET_METHOD(target, "xml2json", ReadXMLStrAsJSONStr);
+  exports->Set(NanNew<String>("xml2json"),
+    NanNew<FunctionTemplate>(ReadXMLFunction)->GetFunction());
 }
 
 NODE_MODULE(xml_parser, init);
